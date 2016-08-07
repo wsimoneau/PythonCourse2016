e4e748b0add8e81384f8c0cccbcb8ae326253108
carlson9 <carlson.dav@gmail.com>
2016-08-07 18:06:59 -0500

    Day1 slides, exercises, and lab

---
+++ Day1/words.py
@@ -0,0 +1,13 @@
+
+def has_no_e(word):
+    """return true or false if there is an e in 'word'"""
+    
+def uses_only(word1,word2):
+    """return true/false if word1 contains only letters from word2"""
+
+def uses_all(word1, word2):
+    """return true/false if word1 uses all the letters in word2"""
+
+def is_abecedarian(word):
+    """is the word in alphabetical order?"""
+    
