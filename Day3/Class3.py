54a69aad57a6d05ee9447a43452f857fbd63415d
carlson9 <carlson.dav@gmail.com>
2016-08-11 15:26:18 -0500

    Slides and labs

--- Day3/Class3.py
+++ Day3/Class3.py
@@ -20,24 +20,4 @@ for i in range(1,10):
 		print i
 else:
 	print "I went through all iterations!"
-	
-def Rename(entry):
-    try:
-     	if entry%1==0:
-    		entry = str(entry)
-    		if entry[-2] == '1':
-    			return "%sth" %(entry)
-     		elif entry[-1] == '1':
-    			return "%sst" %(entry)
-    		elif entry[-1] == '2':
-    			return "%snd" %(entry)
-    		elif entry[-1] == '3':
-    			return "%srd" %(entry)  
-    		else:
-    			return  "%sth" %(entry)	
-    	else:
-    		return "Please do not enter decimal places." 	
-    except:
-    	return "Please enter an integer."
-    finally: 
-    	print "This is a function."
+
